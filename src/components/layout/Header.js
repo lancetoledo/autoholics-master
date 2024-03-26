dfc8b1c5023f38d5f84ae012d943cf041753dd72
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2024-03-26 16:26:37 -0400

    ADD: Refactored code to ensure best practices and ensure proper redirection

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../images/autoholicsLogo.png';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import toast, { Toaster } from 'react-hot-toast';
 import * as Scroll from 'react-scroll';
@@ -21,16 +21,29 @@ const Header = ({ click, setClick, }) => {
     const cartCount = useSelector((state) => state.cart.items.length); // Get the number of items in the cart
     const cartItems = useSelector((state) => state.cart.items);
     const isSidebarVisible = useSelector((state) => state.cart.sidebarVisible);
-
-    let ScrollLink = Scroll.Link;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const ScrollLink = Scroll.Link;
 
     // Logout Function
     const handleLogout = () => {
         dispatch(logout());
         // Optionally, navigate to a different route upon logout
         // navigate('/signin');
+        // Provide a message to let users know they logged out
+        // toast.success('Successfully logged out!', {
+        //     duration: 2000
+        // });
     };
 
+    const redirectToHomeAndScroll = (section) => {
+        if (location.pathname !== '/') {
+            navigate('/');
+        }
+        if (setClick) {
+            setClick(section);
+        }
+    };
 
 
     // UseEffect to scroll user on Header items.
@@ -46,9 +59,6 @@ const Header = ({ click, setClick, }) => {
         return () => window.removeEventListener('scroll', changeNav); // Cleanup
     }, []);
 
-    const handleStatus = (status) => {
-        setClick(status);
-    };
 
     const clearActive = () => {
         setClick(false);
@@ -61,24 +71,22 @@ const Header = ({ click, setClick, }) => {
 
     return (
         <>
-            {/* <Toaster
+            <Toaster
                 position="top-center"
                 reverseOrder={false}
-                containerClassName="toast"
-            /> */}
+                containerClassName="toast-container"
+            />
             <div className='header' style={{ background: scrollNav ? '#010606' : 'transparent' }}>
                 <div className='nav'>
                     <ScrollLink activeClass='active' className='logo' to='hero' spy={true} smooth={true} offset={50} duration={500} onClick={() => clearActive()}>
                         <img src={logo} alt='Autoholics' />
                     </ScrollLink>
                     <div className='nav_menu'>
-                        <ScrollLink className='nav_item' activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500} onClick={() => handleStatus('about')}><p className={click === 'about' ? 'nav_item activate' : 'nav_item'}>About Us</p></ScrollLink>
-                        <ScrollLink className='nav_item' activeClass="active" to="services" spy={true} smooth={true} offset={50} duration={500} onClick={() => handleStatus('services')}><p className={click === 'services' ? 'nav_item activate' : 'nav_item'}>Services</p></ScrollLink>
-                        <ScrollLink className='nav_item' activeClass="active" to="discover" spy={true} smooth={true} offset={50} duration={500} onClick={() => handleStatus('contact')}><p className={click === 'contact' ? 'nav_item activate' : 'nav_item'}>Contact Us</p></ScrollLink>
-                        <Link className='nav_item' to="shop" style={{ textDecoration: 'none', color: 'white' }}>
-                            <p className='nav_item' >Shop</p>
-                        </Link>
-                        {/* <ScrollLink activeClass="active" to="shop" spy={true} smooth={true} offset={50} duration={500} onClick={() => handleStatus('shop')}><p className={'nav_item'}>Shop</p></ScrollLink> */}
+                        {/* Update ScrollLink with new onClick function */}
+                        <ScrollLink className='nav_item' activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500} onClick={() => redirectToHomeAndScroll('about')}>About Us</ScrollLink>
+                        <ScrollLink className='nav_item' activeClass="active" to="services" spy={true} smooth={true} offset={50} duration={500} onClick={() => redirectToHomeAndScroll('services')}>Services</ScrollLink>
+                        <ScrollLink className='nav_item' activeClass="active" to="discover" spy={true} smooth={true} offset={50} duration={500} onClick={() => redirectToHomeAndScroll('contact')}>Contact Us</ScrollLink>
+                        <Link className='nav_item' to="/shop" style={{ textDecoration: 'none', color: 'white' }}>Shop</Link>
                     </div>
                     <div className='user_controls'>
                         {user ? <p className='user'>{user?.displayName}</p> : ''}
@@ -101,7 +109,7 @@ const Header = ({ click, setClick, }) => {
                                 Log Out
                             </div>
                         ) : (
-                            <Link className='btn' id='nav_btn' to='signin' style={{ textDecoration: 'none' }}>
+                            <Link className='btn' id='nav_btn' to='/signin' style={{ textDecoration: 'none' }}>
                                 Sign In
                             </Link>
                         )}
